ae3d237b4287df100e768c8815997e7bb371747e
Eduard <blademan82@gmail.com>
2023-02-23 04:45:29 +0200

    add About and Service section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,5 +1,17 @@
 import styles from './about.module.css';
 
 export default function About() {
-  return <section>About</section>;
+  return (
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <img src="" alt="About Image" />
+        </div>
+      </div>
+      <div className="about__right">
+        <h2>About Me</h2>
+        <div className="about__cards"></div>
+      </div>
+    </section>
+  );
 }
